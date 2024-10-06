a392bd816979d75804a787810d2352db52e45aab
bluekmky1 <115731932+bluekmky1@users.noreply.github.com>
2024-10-06 21:52:50 +0900

    🎨 검색 기능 추가

--- src/utils/getAllNotes.tsx
+++ src/utils/getAllNotes.tsx
@@ -1,96 +1,136 @@
-import { NoteCard } from "../components"
-import { NotesContainer } from "../styles/styles"
-import { Note } from "../types/note"
+import { NoteCard } from "../components";
+import { NotesContainer } from "../styles/styles";
+import { Note } from "../types/note";
 
 const filteredNotes = (notes: Note[], filter: string) => {
+  const lowPriority = notes.filter(({ priority }) => priority === "low");
+  const highPriority = notes.filter(({ priority }) => priority === "high");
 
-    const lowPriority = notes.filter(({ priority }) => priority === "low");
-    const highPriority = notes.filter(({ priority }) => priority === "high");
+  if (filter === "low") {
+    return [...lowPriority, ...highPriority];
+  } else if (filter === "high") {
+    return [...highPriority, ...lowPriority];
+  } else if (filter === "latest") {
+    return notes.sort((a, b) => b.createdTime - a.createdTime);
+  } else if (filter === "created") {
+    return notes.sort((a, b) => a.createdTime - b.createdTime);
+  } else if (filter === "edited") {
+    const editedNotes = notes.filter(({ editedTime }) => editedTime);
+    const normalNotes = notes.filter(({ editedTime }) => !editedTime);
 
-    if (filter === "low") {
-        return [...lowPriority, ...highPriority];
-    } else if (filter === "high") {
-        return [...highPriority, ...lowPriority];
-    } else if (filter === 'latest') {
-        return notes.sort((a, b) => b.createdTime - a.createdTime);
-    } else if (filter === "created") {
-        return notes.sort((a, b) => a.createdTime - b.createdTime);
-    } else if (filter === "edited") {
-        const editedNotes = notes.filter(({ editedTime }) => editedTime);
-        const normalNotes = notes.filter(({ editedTime }) => !editedTime);
+    const sortEdited = editedNotes.sort(
+      (a, b) => (b.editedTime as number) - (a.editedTime as number)
+    );
+    return [...sortEdited, ...normalNotes];
+  } else {
+    return notes;
+  }
+};
 
-        const sortEdited = editedNotes.sort((a, b) => ((b.editedTime as number) - (a.editedTime as number)));
-        return [...sortEdited, ...normalNotes];
-    } else {
-        return notes;
-    }
+const getAllNotes = (
+  mainNotes: Note[],
+  filter: string,
+  searchKeyword: string
+) => {
+  const pinned = mainNotes.filter(({ isPinned }) => isPinned);
+  const normal = mainNotes.filter(({ isPinned }) => !isPinned);
 
-}
+  if (searchKeyword.length != 0) {
+    const searchedPinned = pinned.filter((note) => {
+      return note.title
+        .toLocaleLowerCase()
+        .includes(searchKeyword.toLocaleLowerCase());
+    });
+    const searchedNormal = normal.filter((note) => {
+      return note.title
+        .toLocaleLowerCase()
+        .includes(searchKeyword.toLocaleLowerCase());
+    });
 
+    return (
+      <>
+        <div>
+          <div className="allNotes__notes-type">
+            Searched Pinned Notes <span>({searchedPinned.length})</span>
+          </div>
+          {searchedPinned.length == 0 ? "No search result" : null}
+          <NotesContainer>
+            {filteredNotes(searchedPinned, filter).map((note) => (
+              <NoteCard key={note.id} note={note} type="notes" />
+            ))}
+          </NotesContainer>
+        </div>
+        <div>
+          <div className="allNotes__notes-type">
+            Searched Notes <span>({searchedNormal.length})</span>
+          </div>
+          {searchedNormal.length == 0 ? "No search result" : null}
+          <NotesContainer>
+            {filteredNotes(searchedNormal, filter).map((note) => (
+              <NoteCard key={note.id} note={note} type="notes" />
+            ))}
+          </NotesContainer>
+        </div>
+      </>
+    );
+  }
 
+  if (normal.length !== 0 && pinned.length === 0) {
+    return (
+      <>
+        <div className="allNotes__notes-type">
+          All Notes <span>({normal.length})</span>
+        </div>
+        <NotesContainer>
+          {filteredNotes(normal, filter).map((note) => (
+            <NoteCard key={note.id} note={note} type="notes" />
+          ))}
+        </NotesContainer>
+      </>
+    );
+  }
 
+  if (pinned.length !== 0 && normal.length === 0) {
+    return (
+      <>
+        <div className="allNotes__notes-type">
+          Pinned Notes <span>({pinned.length})</span>
+        </div>
+        <NotesContainer>
+          {filteredNotes(pinned, filter).map((note) => (
+            <NoteCard key={note.id} note={note} type="notes" />
+          ))}
+        </NotesContainer>
+      </>
+    );
+  }
 
-const getAllNotes = (mainNotes: Note[], filter: string) => {
-
-    const pinned = mainNotes.filter(({ isPinned }) => isPinned);
-    const normal = mainNotes.filter(({ isPinned }) => !isPinned);
-
-    if (normal.length !== 0 && pinned.length === 0) {
-        return (
-            <>
-                <div className="allNotes__notes-type">
-                    All Notes <span>({normal.length})</span>
-                </div>
-                <NotesContainer>
-                    {filteredNotes(normal, filter).map((note) => (
-                        <NoteCard key={note.id} note={note} type="notes" />
-                    ))}
-                </NotesContainer>
-            </>
-        )
-    }
-
-    if (pinned.length !== 0 && normal.length === 0) {
-        return (
-            <>
-                <div className="allNotes__notes-type">
-                    Pinned Notes <span>({pinned.length})</span>
-                </div>
-                <NotesContainer>
-                    {filteredNotes(pinned, filter).map((note) => (
-                        <NoteCard key={note.id} note={note} type="notes" />
-                    ))}
-                </NotesContainer>
-            </>
-        )
-    }
-
-    if (pinned.length !== 0 && normal.length !== 0) {
-        return (
-            <>
-                <div>
-                    <div className="allNotes__notes-type">
-                        Pinned Notes <span>({pinned.length})</span>
-                    </div>
-                    <NotesContainer>
-                        {filteredNotes(pinned, filter).map((note) => (
-                            <NoteCard key={note.id} note={note} type="notes" />
-                        ))}
-                    </NotesContainer>
-                </div>
-                <div>
-                    <div className="allNotes__notes-type">
-                        All Notes <span>({normal.length})</span>
-                    </div>
-                    <NotesContainer>
-                        {filteredNotes(normal, filter).map((note) => (
-                            <NoteCard key={note.id} note={note} type="notes" />
-                        ))}
-                    </NotesContainer>
-                </div>
-            </>
-        )
-    }
-}
+  if (pinned.length !== 0 && normal.length !== 0) {
+    return (
+      <>
+        <div>
+          <div className="allNotes__notes-type">
+            Pinned Notes <span>({pinned.length})</span>
+          </div>
+          <NotesContainer>
+            {filteredNotes(pinned, filter).map((note) => (
+              <NoteCard key={note.id} note={note} type="notes" />
+            ))}
+          </NotesContainer>
+        </div>
+        <div>
+          <div className="allNotes__notes-type">
+            All Notes <span>({normal.length})</span>
+          </div>
+          <NotesContainer>
+            {filteredNotes(normal, filter).map((note) => (
+              <NoteCard key={note.id} note={note} type="notes" />
+            ))}
+          </NotesContainer>
+        </div>
+      </>
+    );
+  }
+};
 
 export default getAllNotes;
